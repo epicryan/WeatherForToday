5da7d8008780d4a62d9719d7ab63383bf90d5183
Ryan Wells <rawells14@gmail.com>
2014-12-03 10:40:58 -0600

    Changed General Format of App

---
+++ src/Parser.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import org.jsoup.*;
+import org.jsoup.nodes.*;
+
+class Parser {
+
+    public static Document doc;
+
+    public Parser() throws IOException, InterruptedException {
+        
+    }
+
+    public String getTemperature() throws InterruptedException {
+        
+        
+        
+        
+        return "f";
+    }
+}
--- src/WeatherGUI.java
+++ src/WeatherGUI.java
@@ -11,35 +11,26 @@ import org.jsoup.select.*;
 
 public class WeatherGUI extends javax.swing.JFrame {
 
-    public WeatherGUI() {
+    public WeatherGUI() throws IOException {
         initComponents();
+        Parser p = new Parser();
+        String f = p.getTemperature();
+        System.out.println(f);
     }
-    
-    
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         jButton1 = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
-        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
 
-        jButton1.setText("Weather For Today");
+        jButton1.setText("Refresh");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
-            }
-        });
-
-        jLabel1.setText("Weather by Ryan Wells");
-
-        jButton2.setText("Help");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
             }
         });
 
@@ -48,26 +39,15 @@ public class WeatherGUI extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(88, 88, 88)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jLabel1)
-                            .addComponent(jButton1)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(122, 122, 122)
-                        .addComponent(jButton2)))
-                .addContainerGap(106, Short.MAX_VALUE))
+                .addGap(112, 112, 112)
+                .addComponent(jButton1)
+                .addContainerGap(138, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1)
-                .addGap(38, 38, 38)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(121, Short.MAX_VALUE)
                 .addComponent(jButton1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 35, Short.MAX_VALUE)
-                .addComponent(jButton2)
                 .addContainerGap())
         );
 
@@ -75,41 +55,22 @@ public class WeatherGUI extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        try {
-            String City=JOptionPane.showInputDialog("City");
-            Document divTag = Jsoup.connect("http://weather.ericson.net/weather/forecast/"+City).get();
-            Elements CurrentTempinfo = divTag.select("div#weather-now-temp");
-            String Temp= CurrentTempinfo.toString();
-            Temp=Temp.replace("<div id=\"weather-now-temp\" class=\"weather-color-5\">", "");
-            Temp=Temp.replace("&deg;", "");
-            Temp=Temp.replace("</div>", "");
-            Temp="Current Temperature: "+Temp;
-            JOptionPane.showMessageDialog(null, Temp);
-                   } catch (IOException ex) {
-
-            JOptionPane.showMessageDialog(null, "Error Make sure your City is correct and Check your Internet Connection");
-            Logger.getLogger(WeatherGUI.class.getName()).log(Level.SEVERE, null, ex);
-            
-        }
+        System.out.println("Refresh");
     }//GEN-LAST:event_jButton1ActionPerformed
-
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        JOptionPane.showMessageDialog(null, "If there are Many Different Cities with the same name try Typing a \"-\" after then the States Abbreviation.");
-        JOptionPane.showMessageDialog(null, "Ex: Galveston ------> Galveston-TX");
-        JOptionPane.showMessageDialog(null, "Make sure you are connected to the internet");
-    }//GEN-LAST:event_jButton2ActionPerformed
 
     public static void main(String args[]) {
 
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new WeatherGUI().setVisible(true);
+                try {
+                    new WeatherGUI().setVisible(true);
+                } catch (IOException ex) {
+                    Logger.getLogger(WeatherGUI.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
